da32b0df39c3cd051a664df786f81daa7ebb0e2f
Angelo Melonas <angelomelonas@gmail.com>
2020-11-20 10:04:25 +0000

    Added Fixtures for generating data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,23 +3,86 @@
 namespace App\DataFixtures;
 
 use App\Entity\BlogPost;
-use DateTime;
+use App\Entity\Comment;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordEncoderInterface $userPasswordEncoder;
+    private Generator $faker;
+
+    public function __construct(UserPasswordEncoderInterface $userPasswordEncoder)
+    {
+        $this->userPasswordEncoder = $userPasswordEncoder;
+        $this->faker = Factory::create();
+    }
+
     public function load(ObjectManager $manager)
     {
-        $blogPost = new BlogPost();
-        $blogPost->setTitle('Title');
-        $blogPost->setAuthor('Author');
-        $blogPost->setPublished(new DateTime('2020-11-15 12:00:00'));
-        $blogPost->setContent('Some content');
-        $blogPost->setSlug('first-post');
+        $this->loadUsers($manager);
+        $this->loadBlogPost($manager);
+        $this->loadComments($manager);
+    }
 
-        $manager->persist($blogPost);
+    public function loadBlogPost(ObjectManager $manager)
+    {
+        $user = $this->getReference('user_admin');
 
+        for ($i = 0; $i < 100; $i++) {
+            $blogPost = new BlogPost();
+            $blogPost->setTitle($this->faker->realText(30));
+            $blogPost->setAuthor($user);
+            $blogPost->setPublished($this->faker->dateTimeThisYear);
+            $blogPost->setContent($this->faker->realText());
+            $blogPost->setSlug($this->faker->slug);
+            $this->setReference("blog_post_" . $i, $blogPost);
+
+            $manager->persist($blogPost);
+        }
+
+        $manager->flush();
+    }
+
+    public function loadComments(ObjectManager $manager)
+    {
+        $user = $this->getReference('user_admin');
+
+        for ($i = 0; $i < 100; $i++) {
+            $blogPost = $this->getReference('blog_post_' . $i);
+
+            for ($j = 0; $j < rand(1, 10); $j++) {
+                $comment = new Comment();
+                $comment->setContent($this->faker->realText());
+                $comment->setPublished($this->faker->dateTimeThisYear);
+                $comment->setAuthor($user);
+                $comment->setBlogPost($blogPost);
+
+                $manager->persist($comment);
+            }
+        }
+
+        $manager->flush();
+    }
+
+    public function loadUsers(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('Admin');
+        $user->setEmail('admin@blogpostcom');
+        $user->setName('Angelo Melonas');
+        $user->setPassword($this->userPasswordEncoder->encodePassword(
+            $user,
+            'password123'
+        ));
+
+        $this->addReference('user_admin', $user);
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
